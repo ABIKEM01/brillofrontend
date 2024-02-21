ab77d7161cc2d70020b79aaf83477d42b343b240
abikem01 <babayod@yahoo.com>
2024-02-21 12:43:13 +0100

    log connected

--- src/components/cards/ProfileCard.js
+++ src/components/cards/ProfileCard.js
@@ -18,9 +18,9 @@ const ProfileCard = () => {
             {loading && <div>Loading...</div>}
             {error && <div>Error: {error}</div>}
             {success && (
-                <div className="flex flex-col justify-between">
+                <div className="flex flex-row justify-between">
                     {users.map(user => (
-                        <div key={user._id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div key={user._id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 px-4 mx-8">
                             <div className="flex justify-end px-4 pt-4">
                             </div>
                             <div className="flex flex-col items-center pb-10">
@@ -30,7 +30,7 @@ const ProfileCard = () => {
                                     alt="Bonnie image"
                                 />
                                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{user?.username}</h5>
-                                <span className="text-sm text-gray-500 dark:text-gray-400">Interests: {user?.email}</span>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">Email: {user?.email}</span>
                                 <div className="flex mt-4 md:mt-6">
                                     <a
                                         href="#"
